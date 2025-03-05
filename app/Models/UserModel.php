c679131f2aabdc423a0218ebd488acc35d2214f7
soulqan <soultan.agnar@gmail.com>
2025-03-05 09:15:10 +0700

    $fillable

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,10 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use function Laravel\Prompts\password;
+
 class UserModel extends Model
 {
     use HasFactory;
 
     protected $table = 'm_user';  
-    protected $primaryKey = 'user_id'; 
+    protected $primaryKey = 'user_id';
+
+
+    protected $fillable = ['level_id','username', 'nama'];
 }
