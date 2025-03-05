843a4e3051e5526ef589dfd9c7a4e25391cd65d9
soulqan <soultan.agnar@gmail.com>
2025-03-05 09:47:54 +0700

    Not Found Exceptions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,9 +22,10 @@ class UserController extends Controller
         // $user = UserModel::find(1); 
         // $user = UserModel::where('level_id',)->first(); 
         // $user = UserModel::firstWhere('level_id',1); 
-        $user = UserModel::findOr(20,['username', 'nama'], function(){
-            abort(404);
-        }); 
+        // $user = UserModel::findOr(20,['username', 'nama'], function(){
+        //     abort(404);
+        // }); 
+        $user = UserModel::where('username', 'manager')->firstOrfail(); 
 
         return view('user', ['data' => $user]);
     }
