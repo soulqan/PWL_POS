9349f3d981cbf18d67a77c245f3108f533d117ed
soulqan <soultan.agnar@gmail.com>
2025-04-16 09:54:36 +0700

    profile

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,4 +1,20 @@
 <div class="sidebar">
+    <!-- Sidebar user panel (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+            @php
+                $user = auth()->user();
+                $imagePath = $user->profile_photo 
+                    ? asset('storage/' . $user->profile_photo) 
+                    : asset('dist/img/user2-160x160.jpg');
+            @endphp
+            <img src="{{ $imagePath }}" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+            <a href="{{url('/user/edit_foto')}}" class="d-block">{{ $user->username }}</a>
+        </div>
+    </div>
+    
     <!-- SidebarSearch Form -->
     <div class="form-inline mt-2">
         <div class="input-group" data-widget="sidebar-search">
