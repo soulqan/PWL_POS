8ab8e97d7cfc789e597ed071469935cca5eedbf5
soulqan <soultan.agnar@gmail.com>
2025-04-29 18:51:49 +0700

    praktikum3 dan 2

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
